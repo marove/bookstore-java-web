a2f6c9670a3b8e5ba27030e8885296fa75aedd16
Manuel Romero <manuel.romero@innocv.com>
2021-01-05 21:46:47 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Book store
+Same web application developed with different technological stacks of java.
---
+++ src/main/java/es/manuel/bookstore/HelloApplication.java
@@ -0,0 +1,9 @@
+package es.manuel.bookstore;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class HelloApplication extends Application {
+
+}
